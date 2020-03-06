d4b48f912cde84747a3177d4dc005987b0df9687
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-03-07 00:55:38 +0700

    Release v0.001

--- lib/App/ProveMods.pm
+++ lib/App/ProveMods.pm
@@ -37,7 +37,44 @@ You can run with `--dry-run` (`-n`) option first to not actually run `prove` but
 just see what distributions will get tested. An example output:
 
     % prove-mods '^Regexp::Pattern' -n
-    ...
+    prove-mods: Found module: Regexp::Pattern (dist=Regexp-Pattern)
+    prove-mods: Found module: Regexp::Pattern::CPAN (dist=Regexp-Pattern-CPAN)
+    prove-mods: Found module: Regexp::Pattern::Example (dist=Regexp-Pattern)
+    prove-mods: Found module: Regexp::Pattern::Git (dist=Regexp-Pattern-Git)
+    prove-mods: Found module: Regexp::Pattern::JSON (dist=Regexp-Pattern-JSON)
+    prove-mods: Found module: Regexp::Pattern::License (dist=Regexp-Pattern-License)
+    prove-mods: Found module: Regexp::Pattern::License::Parts (dist=Regexp-Pattern-License)
+    prove-mods: Found module: Regexp::Pattern::Net (dist=Regexp-Pattern-Net)
+    prove-mods: Found module: Regexp::Pattern::OS (dist=Regexp-Pattern-OS)
+    prove-mods: Found module: Regexp::Pattern::Path (dist=Regexp-Pattern-Path)
+    prove-mods: Found module: Regexp::Pattern::RegexpCommon (dist=Regexp-Pattern-RegexpCommon)
+    prove-mods: Found module: Regexp::Pattern::Test::re_engine (dist=Regexp-Pattern-Test-re_engine)
+    prove-mods: Found module: Regexp::Pattern::Twitter (dist=Regexp-Pattern-Twitter)
+    prove-mods: Found module: Regexp::Pattern::YouTube (dist=Regexp-Pattern-YouTube)
+    prove-mods: Found dist: Regexp-Pattern
+    prove-mods: Found dist: Regexp-Pattern-CPAN
+    prove-mods: Found dist: Regexp-Pattern-Git
+    prove-mods: Found dist: Regexp-Pattern-JSON
+    prove-mods: Found dist: Regexp-Pattern-License
+    prove-mods: Found dist: Regexp-Pattern-Net
+    prove-mods: Found dist: Regexp-Pattern-OS
+    prove-mods: Found dist: Regexp-Pattern-Path
+    prove-mods: Found dist: Regexp-Pattern-RegexpCommon
+    prove-mods: Found dist: Regexp-Pattern-Test-re_engine
+    prove-mods: Found dist: Regexp-Pattern-Twitter
+    prove-mods: Found dist: Regexp-Pattern-YouTube
+    prove-mods: [DRY] [1/12] Running prove for distribution Regexp-Pattern (directory /home/u1/repos/perl-Regexp-Pattern) ...
+    prove-mods: [DRY] [2/12] Running prove for distribution Regexp-Pattern-CPAN (directory /home/u1/repos/perl-Regexp-Pattern-CPAN) ...
+    prove-mods: [DRY] [3/12] Running prove for distribution Regexp-Pattern-Git (directory /home/u1/repos/perl-Regexp-Pattern-Git) ...
+    prove-mods: [DRY] [4/12] Running prove for distribution Regexp-Pattern-JSON (directory /home/u1/repos/perl-Regexp-Pattern-JSON) ...
+    prove-mods: [DRY] [5/12] Running prove for distribution Regexp-Pattern-License (directory /tmp/hEa7jnla5M/Regexp-Pattern-License-v3.2.0) ...
+    prove-mods: [DRY] [6/12] Running prove for distribution Regexp-Pattern-Net (directory /home/u1/repos/perl-Regexp-Pattern-Net) ...
+    prove-mods: [DRY] [7/12] Running prove for distribution Regexp-Pattern-OS (directory /home/u1/repos/perl-Regexp-Pattern-OS) ...
+    prove-mods: [DRY] [8/12] Running prove for distribution Regexp-Pattern-Path (directory /home/u1/repos/perl-Regexp-Pattern-Path) ...
+    prove-mods: [DRY] [9/12] Running prove for distribution Regexp-Pattern-RegexpCommon (directory /home/u1/repos/perl-Regexp-Pattern-RegexpCommon) ...
+    prove-mods: [DRY] [10/12] Running prove for distribution Regexp-Pattern-Test-re_engine (directory /home/u1/repos/perl-Regexp-Pattern-Test-re_engine) ...
+    prove-mods: [DRY] [11/12] Running prove for distribution Regexp-Pattern-Twitter (directory /home/u1/repos/perl-Regexp-Pattern-Twitter) ...
+    prove-mods: [DRY] [12/12] Running prove for distribution Regexp-Pattern-YouTube (directory /home/u1/repos/perl-Regexp-Pattern-YouTube) ...
 
 The above example shows that I have the distribution directories locally on my
 `~/repos`, except for one 'Regexp::Pattern::License'.
@@ -47,6 +84,11 @@ run `prove` on each directory and provide a summary at the end. Example output:
 
     % prove-mods '^Regexp::Pattern'
     ...
+    +-----------------------------------------------+-------------------------------------+-----------------------------------+--------+
+    | dir                                           | label                               | reason                            | status |
+    +-----------------------------------------------+-------------------------------------+-----------------------------------+--------+
+    | /tmp/2GOBZuxird/Regexp-Pattern-License-v3.2.0 | distribution Regexp-Pattern-License | Test failed (Failed 1/2 subtests) | 500    |
+    +-----------------------------------------------+-------------------------------------+-----------------------------------+--------+
 
 The above example shows that one distribution failed testing. You can scroll up
 for the detailed `prove` output to see the details of failure failed, fix
@@ -70,7 +112,7 @@ _
             summary => 'Module names to prove',
             'x.name.is_plural' => 1,
             'x.name.singular' => 'module',
-            schema => ['array*', of=>'perl::modname*'],
+            schema => ['array*', of=>'re*'],
             req => 1,
             pos => 0,
             greedy => 1,
@@ -102,6 +144,7 @@ sub prove_mods {
 
     App::ProveDists::prove_dists(
         hash_subset(\%args, \%App::ProveDists::args_common),
+        -dry_run => $args{-dry_run},
         _res => [200, "OK", \@included_recs],
     );
 }
